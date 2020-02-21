f9fdc0679670dc7a9652edf4152e5e4923b10f83
chrisberry4545 <chrisberry4545@googlemail.com>
2020-02-21 17:57:42 +0000

    Updates

---
+++ precache-manifest.5a83bd8a0f240c344989953eb75bd5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2756f0785a1b5fca3f3e6d238c232bc",
+    "url": "/switchcraft-api-demo/index.html"
+  },
+  {
+    "revision": "227e5cad84df0399df21",
+    "url": "/switchcraft-api-demo/static/css/main.947e3c83.chunk.css"
+  },
+  {
+    "revision": "ec14de13e1d227b2aa57",
+    "url": "/switchcraft-api-demo/static/js/2.37f47b20.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/switchcraft-api-demo/static/js/2.37f47b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227e5cad84df0399df21",
+    "url": "/switchcraft-api-demo/static/js/main.1c6cd4fc.chunk.js"
+  },
+  {
+    "revision": "cf73dbd8a1ce5958a77c",
+    "url": "/switchcraft-api-demo/static/js/runtime-main.74b67617.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/switchcraft-api-demo/precache-manifest.0c20f89c24b9654c2816d3a155cd4cdd.js"
+  "/switchcraft-api-demo/precache-manifest.5a83bd8a0f240c344989953eb75bd5f6.js"
 );
 
 self.addEventListener('message', (event) => {
