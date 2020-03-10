e6a236335d0c5773c81c2caba1df5392f031593c
chrisberry4545 <chrisberry4545@googlemail.com>
2020-03-10 12:57:11 +0000

    added a loading animation

--- src/components/general/LoadingSpinner.js
+++ src/components/general/LoadingSpinner.js
@@ -3,6 +3,8 @@ import './LoadingSpinner.css';
 
 export const LoadingSpinner = ({
   children
-}) => <div className='c-loading'>{
-  children || 'Loading...'
-}</div>;
+}) => <div className='c-loading'>
+  <div className='c-loading__spinner'></div>
+  {children || 'Loading...'}
+</div>;
+
