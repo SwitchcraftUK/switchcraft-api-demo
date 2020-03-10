26db98648885ca720738026bd8f4fdc563f7ba46
chrisberry4545 <chrisberry4545@googlemail.com>
2020-03-10 10:14:00 +0000

    auto quote users

--- src/components/demo/Quote.js
+++ src/components/demo/Quote.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ENERGY_QUOTE_BACKEND_URL } from '../../config';
 import { LoadingSpinner } from '../general/LoadingSpinner';
 import './Quote.css';
-import { Button } from '../general/Button';
 import { makeRequest } from '../../utils/make-request';
 import { Link } from 'react-router-dom';
 import { useStateValue } from '../general/State';
@@ -39,28 +38,24 @@ export const Quote = () => {
     setIsLoadingState(false);
   };
 
+  useEffect(() => {
+    getQuoteResult()
+  }, []);
+
   const showSwitchButton = quote && quote.tariffs && quote.tariffs.length > 0;
   const cheapestQuote = showSwitchButton && quote.tariffs[0];
   return (
     <div className={`c-quote${
-      showSwitchButton ? ' c-quote__button-shown' : ''
+      showSwitchButton ? ' c-quote--button-shown' : ''
     }`}>
       {
         isLoading
-          ? <LoadingSpinner>Getting your quote...</LoadingSpinner>
+          ? <LoadingSpinner>Checking how much you could save...</LoadingSpinner>
           : showSwitchButton
-            ? <Link className='c-quote__switch-user' to='/switch'>
-              Switch my energy and save £{cheapestQuote.annualSaving.toFixed(0)}
+            ? <Link className='c-quote__switch-user' to='demo/switch'>
+              Switch energy and save £{cheapestQuote.annualSaving.toFixed(0)} a year
             </Link>
-            : (
-              errorState
-                ? <QuoteError errorMessage={errorState} />
-                : <Button onClick={getQuoteResult}>
-                  <span className='c-quote__save-more'>Want to save money on your energy?</span>
-                  <br />
-                  Click here to find out how much you could save
-                </Button>
-            )
+            : errorState && <QuoteError errorMessage={errorState} />
       }
     </div>
   );
